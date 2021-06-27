b4a9b444fff7fd65849ebf4743e37b640100130a
JungWon Park <firstman9@protonmail.com>
2021-06-27 09:34:26 +0900

    추가유형 작성

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from typing import Optional
+from enum import Enum
 
 from fastapi import FastAPI
 from pydantic import BaseModel
@@ -10,6 +11,12 @@ class Item(BaseModel):
     name: str
     price: float
     is_offer: Optional[bool] = None
+
+
+class ModelName(str, Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
 
 
 @app.get("/")
@@ -25,3 +32,29 @@ def read_item(item_id: int, q: Optional[str] = None):
 @app.put("/items/{item_id}")
 def update_item(item_id:int, item: Item):
     return {"item_id": item_id, "item_name": item.name, "item_price": item.price}
+
+
+@app.get("/users/me")
+async def read_user_me():
+    return {"user_id" : "the current user"}
+
+
+@app.get("/users/{user_id}")
+async def read_user(user_id: str):
+    return {"user_id" : user_id}
+
+
+@app.get("/models/{model_name}")
+async def get_model(model_name: ModelName):
+    if model_name == ModelName.alexnet:
+        return {"model_name": model_name, "message": "Deep Learning FTW!"}
+
+    if model_name == ModelName.lenet:
+        return {"model_name": model_name, "message": "LeCNN all the images"}
+
+    return {"model_name": model_name, "message": "Have some residuals"}
+
+
+@app.get("/files/{file_path:path}")
+async def read_file(file_path: str):
+    return {"file_path": file_path}
